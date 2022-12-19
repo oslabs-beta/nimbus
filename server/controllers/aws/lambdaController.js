3407fce4eb3cc34a1696e6a18095ea5ba030a8bc
Zhaowei <sunzhaow0813@gmail.com>
2022-12-19 16:46:55 -0500

    Completed the router connections for function controllers and log controllers

--- server/controllers/aws/lambdaController.js
+++ server/controllers/aws/lambdaController.js
@@ -1,31 +1,50 @@
 "use strict";
-// import { LambdaClient, ListFunctionsCommand } from "@aws-sdk/client-lambda";
-// import { Request, Response, NextFunction } from "express";
-// import dotenv from 'dotenv';
-// dotenv.config();
-// //req: Request, res: Response, next: NextFunction
-// const lambdaController = {
-//     async getFunctions(req: Request, res: Response, next: NextFunction) {
-//         const lambdaClient = new LambdaClient({
-//             region: req.body.region, //'us-east-1',
-//             credentials: req.body.credentials /* {
-//                 accessKeyId: 'ASIAYSDN4ZO5EVRXMD2J',
-//                 secretAccessKey: '2b/KMmCLIeT+zOJwPvCgeRJixlPK9Zo87nfDVGgy',
-//                 sessionToken: 'IQoJb3JpZ2luX2VjEND//////////wEaCXVzLWVhc3QtMSJHMEUCIHZZneS46OGdL10mub0z33xjXAZm8zHL9nqm12CgJwLlAiEAwnSUo8OM9QHicu8CVHJF7pah3laMtvhfL4D6ZwJvD7gqowII+f//////////ARAAGgw1ODg2NDA5OTYyODIiDKGyhOFIpg9FII3NXCr3AS/l40lQFXLbbWg90aLOSVMDrpbfgeFvOPgmHow6WBawBhejChmt/YomqN8wC44TXptymSFoqxPXH+oRUYhsA+3PMumvrN/9iy/8CveI+IUmySxBFUKU95YxxZJEGrP1T4lk2hVKdyY7/jxSbkWsIhH3fPdNDvIXoyQcgxThX9wuXzW/dvv1Q8Uini638alx4PBWG/WarfaO1rSoDpqWI1ZoMUAzbFY8G3ZltkStfCUGLWN8SEwuKn7+Z5c4qU9rZrFZr+0In1KkTWPk5XZwaWxJ4I/NLG/lttA7xnXg94nbDie/vJUVIWRZ+WEsLRCgJg7oaZaQCxMw7uXunAY6nQEns7DO5UvHmsid70gaBpWVfkuwCPyWUTlrdB4PzADCkvjQfdhpxfFsD7+4QK4Wc0kUcQD21chstDpjrLCFCsg3FruPMqxO54RFMyAf3DwS9AMtPS5oSPi2aUNzMttmj3pEMLYYgJnaAxztGzfZncC8w9onpCO0D4G9vAAcd9VVEWsiSFdem4v9tJBn9t4P0aSsEmvxS8hAADMExus8'
-//             } */
-//         });
-//         const getFunctionsCommand = new ListFunctionsCommand({});
-//         try {
-//             const commandResults = await lambdaClient.send(getFunctionsCommand);
-//             const lambdaFunctions = commandResults?.Functions;
-//             const lambdaFunctionDetails = lambdaFunctions?.map(f => f.FunctionName);
-//             console.log(lambdaFunctionDetails);
-//             res.locals.lambdaFunctions = lambdaFunctionDetails;
-//             //return next();
-//         } catch (err) {
-//             console.log(err);
-//             //return next(err);
-//         }
-//     }
-// }
-// lambdaController.getFunctions();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_lambda_1 = require("@aws-sdk/client-lambda");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+//req: Request, res: Response, next: NextFunction
+const lambdaController = {
+    getFunctions(req, res, next) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const lambdaClient = new client_lambda_1.LambdaClient({
+                region: res.locals.region,
+                credentials: res.locals.credentials /* {
+                    accessKeyId: 'ASIAYSDN4ZO5EVRXMD2J',
+                    secretAccessKey: '2b/KMmCLIeT+zOJwPvCgeRJixlPK9Zo87nfDVGgy',
+                    sessionToken: 'IQoJb3JpZ2luX2VjEND//////////wEaCXVzLWVhc3QtMSJHMEUCIHZZneS46OGdL10mub0z33xjXAZm8zHL9nqm12CgJwLlAiEAwnSUo8OM9QHicu8CVHJF7pah3laMtvhfL4D6ZwJvD7gqowII+f//////////ARAAGgw1ODg2NDA5OTYyODIiDKGyhOFIpg9FII3NXCr3AS/l40lQFXLbbWg90aLOSVMDrpbfgeFvOPgmHow6WBawBhejChmt/YomqN8wC44TXptymSFoqxPXH+oRUYhsA+3PMumvrN/9iy/8CveI+IUmySxBFUKU95YxxZJEGrP1T4lk2hVKdyY7/jxSbkWsIhH3fPdNDvIXoyQcgxThX9wuXzW/dvv1Q8Uini638alx4PBWG/WarfaO1rSoDpqWI1ZoMUAzbFY8G3ZltkStfCUGLWN8SEwuKn7+Z5c4qU9rZrFZr+0In1KkTWPk5XZwaWxJ4I/NLG/lttA7xnXg94nbDie/vJUVIWRZ+WEsLRCgJg7oaZaQCxMw7uXunAY6nQEns7DO5UvHmsid70gaBpWVfkuwCPyWUTlrdB4PzADCkvjQfdhpxfFsD7+4QK4Wc0kUcQD21chstDpjrLCFCsg3FruPMqxO54RFMyAf3DwS9AMtPS5oSPi2aUNzMttmj3pEMLYYgJnaAxztGzfZncC8w9onpCO0D4G9vAAcd9VVEWsiSFdem4v9tJBn9t4P0aSsEmvxS8hAADMExus8'
+                } */
+            });
+            const getFunctionsCommand = new client_lambda_1.ListFunctionsCommand({});
+            try {
+                const commandResults = yield lambdaClient.send(getFunctionsCommand);
+                const lambdaFunctions = commandResults === null || commandResults === void 0 ? void 0 : commandResults.Functions;
+                const lambdaFunctionDetails = lambdaFunctions === null || lambdaFunctions === void 0 ? void 0 : lambdaFunctions.map(f => f.FunctionName);
+                console.log(lambdaFunctionDetails);
+                res.locals.functions = lambdaFunctionDetails;
+                return next();
+            }
+            catch (err) {
+                console.log(err);
+                return next({
+                    log: "Error caught in lambdaController.getFunctions middleware function",
+                    status: 500,
+                    message: { errMessage: `Error getting functions for the account`, errors: err }
+                });
+            }
+        });
+    }
+};
+exports.default = lambdaController;
--- server/routes/dashboardRouter.js
+++ server/routes/dashboardRouter.js
@@ -1,13 +1,41 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
-// Give our express app the ability define routes, handle requests, and configure the router by creating an instance of an Express Router
 const router = express.Router();
-router.post('/home', (req, res) => {
-    return res.status(200).send();
+const authController = require('../controllers/authController');
+const credentialsController_1 = __importDefault(require("../controllers/aws/credentialsController"));
+const lambdaController_1 = __importDefault(require("../controllers/aws/lambdaController"));
+const logsController_1 = __importDefault(require("../controllers/aws/logsController"));
+// All routes verify JWT Token to get email
+// Email is used to query the database for ARN
+// ARN is used to get credentials from client's AWS account
+// Credentials used to grab matrics
+// router.post('/home', authController.verifyToken, credentialsController.getCredentialsFromDB, (req: Request, res: Response) => {
+//     return res.status(200).json();
+// });
+router.post('/functions', authController.verifyToken, credentialsController_1.default.getCredentialsFromDB, lambdaController_1.default.getFunctions, (req, res) => {
+    return res.status(200).json({
+        functions: res.locals.functions
+    });
 });
-router.post('/functions');
-router.post('/getlogs');
-router.post('/getmetrics');
-router.post('/apis');
-router.post('/settings');
+router.post('/allLogs', authController.verifyToken, credentialsController_1.default.getCredentialsFromDB, logsController_1.default.getAllLogs, (req, res) => {
+    return res.status(200).json({
+        logs: res.locals.logs
+    });
+});
+router.post('/filteredLogs', authController.verifyToken, credentialsController_1.default.getCredentialsFromDB, logsController_1.default.getFilteredLogs, (req, res) => {
+    return res.status(200).json({
+        filteredLogs: res.locals.filteredLogs
+    });
+});
+// // Add middleware for API Gateway
+// router.post('/apis', authController.verifyToken, credentialsController.getArnFromDB, (req: Request, res: Response) => {
+//     return res.status(200).json();
+// });
+// router.post('/settings', authController.verifyToken, (req: Request, res: Response) => {
+//     return res.status(200).json();
+// });
+module.exports = router;
