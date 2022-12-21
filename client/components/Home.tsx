cec443fe8a86dc32b7b6b2fd6949822625c9e692
Madeline Doctor <maddoctor99@gmail.com>
2022-12-20 20:35:13 -0500

    Successfully grabbed functions metrics from backend, Charts WORK IN PROGRESS

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [invocationsData, setInvocations] = useState({});
+  const [errorsData, setErrors] = useState({});
+  const [throttlesData, setThrottles] = useState({});
+  const [durationData, setDurations] = useState({});
+
+  const route = '/dashboard/allMetrics'
+
+  const getMetrics = async () => {
+    let res;
+    try {
+      res = await fetch(route, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'Application/JSON',
+          authorization: `BEARER ${localStorage.getItem('accessToken')}`,
+          refresh: `BEARER ${localStorage.getItem('refreshToken')}`,
+        },
+      });
+      res = await res.json();
+      setInvocations({invocations: res.metrics.invocations,});
+      setErrors({errors: res.metrics.errors });
+      setThrottles({throttles: res.metrics.throttles });
+      setDurations({duration: res.metrics.duration});
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getMetrics();
+  }, []);
+
+
+
+  return (<div>Home
+    <ul>
+      <li></li>
+    </ul>
+  </div>);
 };
 
 export default Home;
