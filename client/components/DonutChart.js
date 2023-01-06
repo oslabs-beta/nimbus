553f3860b36c92838606f3c95aefaaf330b3f918
Zhaowei <sunzhaow0813@gmail.com>
2023-01-06 16:34:33 -0500

    Typescrpts updated and pass run through

--- client/components/DonutChart.js
+++ client/components/DonutChart.js
@@ -6,20 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const chart_js_1 = require("chart.js");
 const react_chartjs_2_1 = require("react-chartjs-2");
+const chroma_js_1 = __importDefault(require("chroma-js"));
 const DonutChart = (props) => {
+    var _a;
     chart_js_1.Chart.register(chart_js_1.ArcElement, chart_js_1.Tooltip, chart_js_1.Legend);
-    console.log(props.rawData);
+    const colors = chroma_js_1.default.scale(['#4f46e5', '#F471B5']).mode('lch').colors((_a = props.rawData.data) === null || _a === void 0 ? void 0 : _a.length);
     return (react_1.default.createElement(react_chartjs_2_1.Doughnut, { data: {
             labels: props.rawData.labels,
             datasets: [
                 {
                     data: props.rawData.data,
-                    borderColor: ['rgb(130,141,248,0.2)'],
-                    backgroundColor: ['rgba(232,99,132,1)',
-                        'rgba(232,211,6,1)',
-                        'rgba(54,162,235,1)',
-                        'rgba(255,159,64,1)',
-                        'rgba(153,102,255,1)'],
+                    borderWidth: [0],
+                    // borderColor: ['#828DF8'],
+                    backgroundColor: colors,
                 }
             ]
         }, options: {
@@ -27,6 +26,10 @@ const DonutChart = (props) => {
             maintainAspectRatio: false,
             plugins: {
                 legend: {
+                    labels: {
+                        boxWidth: 20,
+                        padding: 5
+                    },
                     position: 'right'
                 }
             }
--- client/containers/UserDashboard.js
+++ client/containers/UserDashboard.js
@@ -44,7 +44,17 @@ const Logs_1 = __importDefault(require("../components/Logs"));
 const Apis_1 = __importDefault(require("../components/Apis"));
 const Settings_1 = __importDefault(require("../components/Settings"));
 const UserDashboard = ({ handleUserLogin, toggleTheme }) => {
+    const routes = {
+        userDetails: '/dashboard/userDetails',
+    };
     const [data, setData] = (0, react_1.useState)([]);
+    const [email, setEmail] = (0, react_1.useState)('');
+    const [firstName, setFirstName] = (0, react_1.useState)('');
+    const [lastName, setLastName] = (0, react_1.useState)('');
+    const [password, setPassword] = (0, react_1.useState)('');
+    const [confirmation, setConfirmation] = (0, react_1.useState)('');
+    const [arn, setArn] = (0, react_1.useState)('');
+    const [region, setRegion] = (0, react_1.useState)('');
     //   const swapAuthView = () => {
     //     setShowLogin((showLogin) => !showLogin);
     //   }
@@ -67,17 +77,43 @@ const UserDashboard = ({ handleUserLogin, toggleTheme }) => {
         console.log(res);
         setData(res);
     });
+    const getUserDetails = () => __awaiter(void 0, void 0, void 0, function* () {
+        let res;
+        try {
+            res = yield fetch(`${routes.userDetails}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'Application/JSON',
+                    authorization: `BEARER ${localStorage.getItem('accessToken')}`,
+                    refresh: `BEARER ${localStorage.getItem('refreshToken')}`,
+                },
+            });
+            // convert response to JS object
+            res = yield res.json();
+            setEmail(res.email);
+            setFirstName(res.firstName);
+            setLastName(res.lastName);
+            setArn(res.arn);
+            setRegion(res.region);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+    (0, react_1.useEffect)(() => {
+        getUserDetails();
+    }, []);
     return (react_1.default.createElement(react_1.default.Fragment, null,
         react_1.default.createElement(react_router_dom_1.BrowserRouter, null,
             react_1.default.createElement("div", { className: "drawer drawer-mobile" },
                 react_1.default.createElement("input", { id: "my-drawer-2", type: "checkbox", className: "drawer-toggle" }),
                 react_1.default.createElement("div", { className: "drawer-content flex flex-col items-center py-12 relative" },
                     react_1.default.createElement(react_router_dom_1.Routes, null,
-                        react_1.default.createElement(react_router_dom_1.Route, { path: '/', element: react_1.default.createElement(Home_1.default, null) }),
+                        react_1.default.createElement(react_router_dom_1.Route, { path: '/', element: react_1.default.createElement(Home_1.default, { firstName: firstName }) }),
                         react_1.default.createElement(react_router_dom_1.Route, { path: 'functions', element: react_1.default.createElement(Functions_1.default, null) }),
                         react_1.default.createElement(react_router_dom_1.Route, { path: 'logs', element: react_1.default.createElement(Logs_1.default, null) }),
                         react_1.default.createElement(react_router_dom_1.Route, { path: 'apis', element: react_1.default.createElement(Apis_1.default, null) }),
-                        react_1.default.createElement(react_router_dom_1.Route, { path: 'settings', element: react_1.default.createElement(Settings_1.default, null) })),
+                        react_1.default.createElement(react_router_dom_1.Route, { path: 'settings', element: react_1.default.createElement(Settings_1.default, { email: email, firstName: firstName, lastName: lastName, password: password, confirmation: confirmation, arn: arn, region: region, setEmail: setEmail, setFirstName: setFirstName, setLastName: setLastName, setPassword: setPassword, setConfirmation: setConfirmation, setArn: setArn, setRegion: setRegion }) })),
                     react_1.default.createElement("label", { htmlFor: "my-drawer-2", className: "btn btn-ghost btn-circle drawer-button lg:hidden absolute left-2 top-2" },
                         react_1.default.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", className: "h-5 w-5", fill: "none", viewBox: "0 0 24 24", stroke: "currentColor" },
                             react_1.default.createElement("path", { strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "2", d: "M4 6h16M4 12h16M4 18h7" })))),
